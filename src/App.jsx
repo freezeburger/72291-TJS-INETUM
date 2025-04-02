24c64a5d71178944c7d33581399c9c83d64be8c6
Your Name <renaud.dubuis@decryptage.net>
2025-04-02 10:06:29 +0200

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import BaseButton from "./components/BaseButton/BaseButton"
 
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import ViewLogin from "./views/ViewLogin";
+import ViewProducts from "./views/ViewProducts";
+import ViewDetail from "./views/ViewDetail";
 
 function App() {
 
   return (
     <>
-      <BaseButton />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ViewLogin />} />
+          <Route path="/products" element={<ViewProducts />} />
+          <Route path="/products/:id" element={<ViewDetail />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
